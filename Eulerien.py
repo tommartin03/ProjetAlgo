dcb1cc6a41ca0e913293258d03cb2fc2458bdd69
kaliyu <tom.roy@sfr.fr>
2024-10-15 21:27:12 +0200

    ajout de la fonction TestCycleEulerien et d'une fonction pour calculer le nb d'arete

--- Eulerien.py
+++ Eulerien.py
@@ -53,11 +53,42 @@ def VerifGrapheEulerien(G):
     return True
 
 def TestCycleEulerien(G, C):
-   """
+    """
     Vérifie si la liste C est un cycle eulérien de G
     """
-   #Todo
-   return None
+    est_passe = [] #liste ou l'on met toutes les arêtes ou l'on est passé
+    for i in range (len(C)-1): 
+        #on vérifie que C[i+1] est bien voisin de C[i] dans le graphe
+        if C[i+1] in G[C[i]]:
+            #on verifie qu'on a pas déjà pris cette arêtes 
+            if [C[i],C[i+1]] not in est_passe and [C[i+1],C[i]] not in est_passe:
+                est_passe.append([C[i],C[i+1]])
+            else:
+                print([C[i],C[i+1]])
+                return False
+        else:
+            print(C[i+1],",", G[C[i]])
+            return False
+    #on verifie qu'on est bien passé par toute les arêtes 
+    if(len(est_passe) == nombre_aretes(G)):
+        return True
+    else:
+        
+        return False
+     
+def nombre_aretes(graphe):
+    """
+    Calcule le nombre d'arêtes dans un graphe.
+    
+    :param graphe: Dictionnaire représentant le graphe sous forme d'adjacence.
+    :return: Nombre d'arêtes.
+    """
+    count = 0
+    for sommet, adjacents in graphe.items():
+        count += len(adjacents)
+    
+    # Comme le graphe est non orienté, chaque arête est comptée deux fois.
+    return count // 2
 
 def EstIsthme(G, e):
     """
@@ -79,6 +110,8 @@ def lire_graphe_json(fichier):
 
 def main():
     graphe = lire_graphe_json("graphes.json")
+    C = [1, 2, 4, 3, 2, 5, 1,6,7,1]
+    print(TestCycleEulerien(graphe,C))
     print(VerifGrapheEulerien(graphe))
 
 if __name__ == "__main__":
